6709a733c1a9a071e12f26e2cd92f5dc0622fcc4
LucaTrandafir <96697844+LucaTrandafir@users.noreply.github.com>
2022-04-04 19:31:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,51 +14,46 @@ app = FastAPI()
 
 
 @app.get("/group/promote/")
-async def read_items(user_name: str, key: str, groupid: int, role_number: int, cookie: str):
+async def read_items(user_name: str, groupid: int, role_number: int, cookie: str):
     client = Client(cookie)
     
-    if key == APIKEY:
+
      group = await client.get_group(groupid)
      usernameinsystem = await client.get_user_by_username(user_name)
      user_id = usernameinsystem.id
      membertorank =  await group.get_member_by_id(user_id)
      await membertorank.promote()
      return ("The user was promoted!")
-    else:
-        return "Incorrect key"
+
 
 @app.get("/group/demote/")
-async def read_items(user_name: str, key: str, groupid: int, role_number: int, cookie: str):
+async def read_items(user_name: str, groupid: int, role_number: int, cookie: str):
     client = Client(cookie)
     
-    if key == APIKEY:
+ 
      group = await client.get_group(groupid)
      usernameinsystem = await client.get_user_by_username(user_name)
      user_id = usernameinsystem.id
      membertorank =  await group.get_member_by_id(user_id)
      await membertorank.demote()
      return ("The user was demoted!")
-    else:
-        return "Incorrect key"
+
 
 @app.get("/group/rank/")
-async def read_items(user_name: str, key: str, groupid: int, role_number: int, cookie: str):
+async def read_items(user_name: str, groupid: int, role_number: int, cookie: str):
     client = Client(cookie)
     
-    if key == APIKEY:
+
      group = await client.get_group(groupid)
      target = await group.get_member_by_username(user_name)
      await target.setrole(role_number)
      return ("The user had their ranked changed")
-    else:
-        return "Incorrect key"
+ 
 
 @app.get("/group/members/")
-async def read_items(user_name: str, key: str, groupid: int, cookie: str):
+async def read_items(user_name: str, groupid: int, cookie: str):
     client = Client(cookie)
     
-    if key == APIKEY:
      group = await client.get_group(groupid)
      return (group.member_count)
-    else:
-        return "Incorrect key"
+
